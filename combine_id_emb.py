3fed5e22db9fe302426196b94e2647146d9bcaee
Leo Bright <564312023@qq.com>
2020-05-21 10:11:53 +0800

    增加kpca embeddings和osmid合并的代码

---
+++ combine_id_emb.py
@@ -0,0 +1,39 @@
+
+
+def combine_id_emb(raw_emb, kpca_emb, output):
+
+    osmids = []
+    embeddings = []
+    combined_embeddings = []
+
+    with open(raw_emb) as f:
+        for line in f:
+            id_ = line.strip().split(' ')
+            osmids.append(id_[0])
+
+    with open(kpca_emb) as f:
+        for line in f:
+            emb = line.strip().split(' ')
+            embeddings.append(emb)
+
+    assert len(osmids) == len(embeddings)
+
+    for i in range(len(osmids)):
+        _id = []
+        _id.append(osmids[i])
+        _combined_emb = _id + embeddings[i]
+        combined_embeddings.append(_combined_emb)
+
+    with open(output, 'w+') as f:
+        for emb in combined_embeddings:
+            f.write(' '.join(emb))
+            f.write('\n')
+
+
+if __name__ == '__main__':
+
+    raw_feature_path = 'sanfrancisco/sanfrancisco_raw_feature_crossing.embeddings'
+    kpca_emb_path = 'sanfrancisco/sanfrancisco_kpca_crossing.embeddings'
+    combined_emb_path =  'sanfrancisco/sanfrancisco_combined_kpca_crossing.embeddings'
+
+    combine_id_emb(raw_feature_path, kpca_emb_path, combined_emb_path)
